d20603e742dd3ee59eb4ff14edda1543ab2fe88a
eraycaylak <eraycayylak1@gmail.com>
2026-01-29 04:24:59 +0300

    Config: Add external image domains for imgur, ibb, cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'imgur.com' },
+      { protocol: 'https', hostname: 'i.ibb.co' },
+      { protocol: 'https', hostname: 'cdn.discordapp.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: '*.telegram.org' },
+      { protocol: 'https', hostname: 't.me' },
+    ],
+  },
 };
 
 export default nextConfig;
